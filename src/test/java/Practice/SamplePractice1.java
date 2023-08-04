ba5fa927fe2afe45343d36fd63e071656ce5831c
abhijeet <abhijeet@Abhi>
2023-08-04 17:04:39 +0530

    SamplePractice class commited

---
+++ src/test/java/Practice/SamplePractice1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class SamplePractice1 {
+	
+	@Test
+	public void demo1()
+	{
+		int a=20;
+		int b=30;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
